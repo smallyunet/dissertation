9a4d6f4e72e2314a0d2c7eea580f5fa83a74bb9d
smallyunet <499503799@qq.com>
2019-04-13 22:42:08 +0800

    som

--- SQL/article.sql
+++ SQL/article.sql
@@ -2,6 +2,8 @@ CREATE TABLE article (
 	id int NOT NULL AUTO_INCREMENT COMMENT "博文ID",
 	title varchar(255) NOT NULL COMMENT "博文标题",
 	content text NOT NULL COMMENT "博文内容",
+	user_id int DEFAULT 1 COMMENT "用户ID",
+	blog_id int NOT NULL COMMENT "博客ID",
 	gmt_create timestamp DEFAULT '0000-00-00 00:00:00' COMMENT "创建时间",
 	gmt_modified timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT "更新时间",
 	PRIMARY KEY (id)
